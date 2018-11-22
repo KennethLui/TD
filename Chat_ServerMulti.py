c73e49728fadbb077d46fe2df9092b7ebf2397e5
KennethLui <kenneth071195@gmail.com>
2018-11-22 21:31:15 -0200

    Add files via upload

--- Chat_ServerMulti.py
+++ Chat_ServerMulti.py
@@ -16,37 +16,41 @@ serverSocket.listen(10)
 
 clientes={}
 
-def buscar():
+def buscar_sala():
+    return
 
-def entrar():
+def entrar_sala():
+    return
 
-def criar():
+def criar_sala():
+    return
 
-def apagar():
+def apagar_sala():
+    return
 
 def menu(conexao):
     msg = 'Menu:\n1 - Buscar informações sobre as salas\n2 - Entrar em e sair de uma sala existente\n3 - Criação de salas públicas e privadas\n4 - Apagar salas\n5 - Sair e desligar o servidor\n\nOpção: '
     erro = 'Resposta inválida. Escolha uma das opções de 1 a 5\n\nOpção: '
     sair = 'sair'
     conexao.send(msg.encode())
-    flag = True
-    while flag = True
+    flag = False
+    while not flag:
         resposta = conexao.recv(1024)
-        if resposta == 1:
-            buscar()
-            flag = False
-        elif resposta == 2:
-            entrar()
-            flag = False
-        elif resposta == 3:
-            criar()
-            flag = False
-        elif resposta == 4:
-            apagar()
-            flag = False
-        elif resposta == 5:
+        if resposta == '1':
+            buscar_sala()
+            flag = True
+        elif resposta == '2':
+            entrar_sala()
+            flag = True
+        elif resposta == '3':
+            criar_sala()
+            flag = True
+        elif resposta == '4':
+            apagar_sala()
+            flag = True
+        elif resposta == '5':
             conexao.send(sair.encode())
-            flag = False
+            flag = True
         else:
             conexao.send(erro.encode())
 
@@ -57,17 +61,17 @@ def cadastro(conexao,endereco):
     cadastrado = False
     while not cadastrado:
         msg1 = 'Já possui cadastro?\n1 - Sim\n2 - Não\n\nResposta: '
-        msg2 = 'Digite o apelido desejado\nApelido: '
-        msg3 = 'Cadastro efetuado'
-        msg4 = 'Noma já cadastrado'
-        msg5 = 'Realizar cadastro'
+        msg2 = '\nDigite o apelido desejado\nApelido: '
+        msg3 = '\nCadastro efetuado\n'
+        msg4 = '\nNoma já cadastrado\n'
+        msg5 = '\nRealizar cadastro\n'
         conexao.send(msg1.encode())
 
-        resposta = conexao.recv(1024)
-
-        if resposta==1:
+        resposta = conexao.recv(1024).decode('utf-8')
+        print('\nResposta recebida do usuário: ',resposta)
+        if resposta=='1':
             cadastrado = True
-        else:
+        elif resposta=='2':
             print('Resposta de cadastro ainda não efetuado')
             cadastrado = True
             conexao.send(msg5.encode())
@@ -75,6 +79,8 @@ def cadastro(conexao,endereco):
             resposta = conexao.recv(1024)
             clientes.update({endereco:resposta.decode('utf-8')})
             conexao.send(msg3.encode())
+        else:
+            print('\nOpção inválida')
 
 def lidacliente(conexao, endereco):
     time.sleep(5)
@@ -98,10 +104,11 @@ def lidacliente(conexao, endereco):
     conexao.close()
 
     print('Lista de clientes: ')
-    #for addr in clientes:
-    #    print (addr)
-
-    print(clientes.items())
+    print ("{:<24} {:<15}".format('Endereço','Apelido'))
+    for endereco,nome in clientes.items():
+        #print ("{:<15} {:<15}".format(k, clientes[k]))
+        print(endereco,'\t',nome)
+    #print(clientes.items())
 
 def despacha():
 
